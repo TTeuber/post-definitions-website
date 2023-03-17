badaf1c59680a6acd891df22e671c4ca6ebdf3b1
TTeuber <100668008+TTeuber@users.noreply.github.com>
2023-03-17 15:52:27 -0700

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,21 +9,21 @@ export default function Footer() {
         <ul>
           <li>fredericklancia@gmail.com</li>
           <li>
-            <a href="https://www.linkedin.com">
+            <a href="https://www.linkedin.com/in/frederick-lancia-b99598159/">
               {"LinkedIn "}
               <ArrowTopRightOnSquareIcon className={"w-4 inline pb-1"}/>
             </a>
           </li>
           <li className={"whitespace-nowrap"}>
-            <a href="https://www.instagram.com">
+            <a href="https://www.instagram.com/freddielancia/">
               {"Instagram "}
               <ArrowTopRightOnSquareIcon className={"inline w-4 pb-1"}/>
             </a>
           </li>
         </ul>
         <h3 className={"uppercase"}>
-          <span>Site developed by Tyler Teuber</span>
-          <br/>
+          {/*<span>Site developed by Tyler Teuber</span>*/}
+          {/*<br/>*/}
           <span>&#169; Post Definitions</span>
         </h3>
       </div>
