f8f0084c44ae74472a5394ef4d15960c735c5738
mllancia <160145850+mllancia@users.noreply.github.com>
2024-06-07 14:55:09 +0300

    Update index.tsx
    updated cover back to original

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <div className={"h-[50vh] object-center "}>
           <Image
             priority
-            src={"/photos/NewerPCCover.jpg"}
+            src={"/photos/OriginalPodcastCover.JPG"}
             alt={"podcast cover"}
             height={400}
             width={400}
